f306657e94ae5795cce6ae9dc1b795b50da8356e
TK Sun <tksun14@gmail.com>
2017-11-08 10:42:45 +0800

    get preview camera to work. Also remove extra file

--- Gestur/camera.swift
+++ Gestur/camera.swift
@@ -8,8 +8,13 @@
 
 import Foundation
 import UIKit
+import AVFoundation
 
 class cameraController: UIViewController {
+    var session: AVCaptureSession?
+    var stillImageOutput: AVCaptureStillImageOutput?
+    var videoPreviewLayer: AVCaptureVideoPreviewLayer?
+    @IBOutlet weak var previewView: UIView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -21,5 +26,42 @@ class cameraController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        session = AVCaptureSession()
+        session!.sessionPreset = AVCaptureSessionPresetPhoto
+        let backCamera = AVCaptureDevice.defaultDevice(withMediaType: AVMediaTypeVideo)
+        
+        var error: NSError?
+        var input: AVCaptureDeviceInput!
+        do{
+            input = try AVCaptureDeviceInput(device: backCamera)
+        }catch let error1 as NSError{
+            error = error1
+            input = nil
+            print(error!.localizedDescription)
+        }
+        
+        if error == nil && session!.canAddInput(input){
+            session!.addInput(input)
+            // ..
+            // the remainder of the session setup will go here
+        }
+        
+        if session!.canAddOutput(stillImageOutput){
+            session!.addOutput(stillImageOutput)
+        }
+        
+        videoPreviewLayer = AVCaptureVideoPreviewLayer(session: session)
+        videoPreviewLayer!.videoGravity = AVLayerVideoGravityResizeAspect
+        videoPreviewLayer!.connection?.videoOrientation = AVCaptureVideoOrientation.portrait
+        previewView.layer.addSublayer(videoPreviewLayer!)
+        session!.startRunning()
+    }
+   
     
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        videoPreviewLayer!.frame = previewView.bounds
+    }
 }
--- LogIn.swift
+++
@@ -1,9 +0,0 @@
-//
-//  LogIn.swift
-//  Gestur
-//
-//  Created by tk on 08/11/2017.
-//  Copyright © 2017 tk. All rights reserved.
-//
-
-import Foundation
