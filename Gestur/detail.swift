4ad045ecd9a7690f7ee583e92798951af7072e1c
TK Sun <tksun14@gmail.com>
2017-12-10 08:32:07 +0800

    setting page set up

--- Gestur/detail.swift
+++ Gestur/detail.swift
@@ -10,13 +10,23 @@ import Foundation
 import UIKit
 
 class detailController: UIViewController {
+   
+    @IBOutlet var settingTitle: UINavigationBar!
+    @IBOutlet var EnglishButton: UIButton!
+    @IBOutlet var ChineseButton: UIButton!
+    @IBOutlet var SelectButton: UIButton!
+    @IBOutlet var CameraButton: UIBarButtonItem!
     
+    @IBOutlet var buttons: [UIButton]!
     override var preferredStatusBarStyle: UIStatusBarStyle {
         return .lightContent
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        EnglishButton.isEnabled = false
+        EnglishButton.setTitleColor(UIColor.lightGray, for: .disabled)
+        ChineseButton.setTitleColor(UIColor.lightGray, for: .disabled)
         // Do any additional setup after loading the view, typically from a nib.
     }
     
@@ -25,5 +35,34 @@ class detailController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
+    @IBAction func handleSelection(_ sender: UIButton) {
+        buttons.forEach { (button) in
+            UIView.animate(withDuration: 0.3, animations: {
+                button.isHidden = !button.isHidden
+                self.view.layoutIfNeeded()
+            })
+        }
+    }
+    
+    @IBAction func EnglishClicked(_ sender: UIButton) {
+        SelectButton.setTitle("Select a language", for: .normal)
+        ChineseButton.setTitle("Chinese/中文", for: .normal)
+        EnglishButton.setTitle("English", for: .normal)
+        CameraButton.title="Camera"
+        settingTitle.topItem?.title = "Setting"
+        EnglishButton.isEnabled = false
+        EnglishButton.setTitleColor(UIColor.lightGray, for: .disabled)
+        ChineseButton.isEnabled = true
+    }
+    
+    @IBAction func ChineseClicked(_ sender: UIButton) {
+       SelectButton.setTitle("请选择语言", for: .normal)
+        ChineseButton.setTitle("中文", for: .normal)
+        EnglishButton.setTitle("英文／English", for: .normal)
+        CameraButton.title="返回相机"
+        settingTitle.topItem?.title = "设置"
+        ChineseButton.isEnabled = false
+        EnglishButton.isEnabled = true
+    }
     
 }
